fdbc8e2a9d0024ee4cbbc9317e39de8549b9736d
Alex <semer541@gmail.com>
2018-03-05 14:29:46 +0300

    version 1.0

---
+++ ConsoleApplication1/ConsoleApplication1/MyConsoleApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace MyConsolApp
+{
+    class MyConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name please");
+            string name = Convert.ToString(Console.ReadLine());
+            Console.Write("Hellow {0} !!!",name);
+            Console.ReadKey();
+        }    
+    }
+}
